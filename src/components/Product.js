9d19af50a1fa17e9f7650911a8b42cce95967e4a
Yessica Mariesta <114063729+yessicamariesta@users.noreply.github.com>
2023-06-26 20:20:06 +0700

    Add files via upload

---
+++ src/components/Product.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Product.css'
+import { useDispatch } from 'react-redux'
+import { cartActions } from '../store/cart-slice'
+
+const Product = ({ name, id, imgURL, price }) => {
+  const dispatch = useDispatch()
+
+  const addToCart = () => {
+    dispatch(
+      cartActions.addToCart({
+        name,
+        id,
+        price,
+      })
+    )
+  }
+
+  return (
+    <div className='card'>
+      <img src={imgURL} alt={name} />
+      <h2>{name}</h2>
+      <p>$ {price}</p>
+      <button onClick={addToCart}>Add to cart</button>
+    </div>
+  )
+}
+
+export default Product
---
+++ src/store/cart-actions.js
@@ -0,0 +1,73 @@
+import { uiActions } from '../store/ui-slice'
+import { cartActions } from './cart-slice'
+
+export const fetchData = () => {
+  return async (dispatch) => {
+    const fetchHandler = async () => {
+      const res = await fetch(
+        'https://redux-project-7220f-default-rtdb.firebaseio.com/cartItems.json'
+      )
+      const data = await res.json()
+      return data
+    }
+
+    try {
+      const cartData = await fetchHandler()
+      dispatch(cartActions.replaceData(cartData))
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: 'Sending Request Failed',
+          type: 'error',
+        })
+      )
+    }
+  }
+}
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        open: true,
+        message: 'Sending Request',
+        type: 'warning',
+      })
+    )
+
+    const sendRequest = async () => {
+      // Send state as sending request
+
+      const res = await fetch(
+        'https://redux-project-7220f-default-rtdb.firebaseio.com/cartItems.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      )
+      const data = await res.json()
+
+      // Send state as  request is successful
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: 'Sending Request To Database Successfully',
+          type: 'success',
+        })
+      )
+    }
+
+    try {
+      await sendRequest()
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: 'Sending Request Failed',
+          type: 'error',
+        })
+      )
+    }
+  }
+}
